5aeb15fdb9691d850cee512100c47471c3824940
cscs885 <344630726@qq.com>
2021-07-21 23:22:37 +0800

    initial commit

---
+++ app/src/main/java/com/huawei/guess2021cs/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.huawei.guess2021cs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.color.ok;
+        R.string.ok;
+        R.drawable.cat;
+    }
+}
